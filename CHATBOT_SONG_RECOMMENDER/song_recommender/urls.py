d60d01e8c7a33a84d3ffd50050d15bc69b956f7d
kiran poojary <64731026+kiranpoojari@users.noreply.github.com>
2025-06-28 15:16:07 +0530

    Update urls.py

--- CHATBOT_SONG_RECOMMENDER/song_recommender/urls.py
+++ CHATBOT_SONG_RECOMMENDER/song_recommender/urls.py
@@ -3,8 +3,7 @@ from .views import chat_bot_view, index_view, process_chat, process_voice, detec
 
 urlpatterns = [
     path('', index_view, name='index'),
-    path('chatbot/', chat_bot_view, name='chatbot'),  # Correcting this line if 'chatbot' was missing
-    path('process_chat/', process_chat, name='process_chat'),
+    path('chatbot/', chat_bot_view, name='chatbot'),  
     path('process_voice/', process_voice, name='process_voice'),
     path('detect_expression/', detect_expression, name='detect_expression'),
     path('process_emotion/', process_emotion, name='process_emotion'),
